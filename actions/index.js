0f6f6845ff7ca7f82cca37895c298ea589aac3fe
Jan-Vidar Bakke <janvidar@fink.no>
2020-10-14 13:54:53 +0200

    Small cleanup

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,7 @@
-const Future = require ('fluture');
-const { Json } = require ('../lib/fluture-express');
 const S = require ('../lib/sanctuary');
+const { Json } = require ('../lib/fluture-express');
 const { deleteMany } = require ('../lib/db');
 
 module.exports = req => 
-  S.map (result => Json (200) (result))
+  S.map (Json (200))
         (deleteMany ('tull') ({d: 'i'}) ({}));
--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', dispatch ('index'));
+app.get('/', dispatch ('index'));
 
 app.use(handleErrors);
 
